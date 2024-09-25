c26eef03fc2698caebc8bd777efdbed6c8ad45f8
Simon Eke <simon3eke@gmail.com>
2024-09-25 11:08:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace labb5_grunderna_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
