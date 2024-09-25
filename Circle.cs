125483a58acad41cd457b905407248fec96143f1
Simon Eke <simon3eke@gmail.com>
2024-09-25 14:24:54 +0200

    Grunduppgift klar dvs en klass och en main

---
+++ Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labb5_grunderna_OOP
+{
+    class Circle
+    {
+        
+        // private fields declared, radius and pi.
+        private int _radius;
+        private double pi = Math.PI;
+
+        // Constructor for an object if created in Main()
+        public Circle(int inputRadius)
+        {
+            this._radius = inputRadius;
+        }
+
+        // Getter and Setter of _radius
+        public int radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        // Method - Calculates and prints area based of _radius
+        public void GetArea()
+        {
+            // Calculates the area r^2*pi
+            double areaOfCircle = (_radius ^ 2) * pi;
+
+            // Prints a string where {0} is the radius and {1:###.###} is the area with 3 decimal places
+            Console.WriteLine("The area of the circle with radii {0} is {1:###.###} \n({1})\n" , _radius, areaOfCircle);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // One object is created with radius 5
+            // Calculates and prints area
+            Circle areaOfCircle1 = new Circle(5);
+            areaOfCircle1.GetArea();
+
+            // Sets the radius for areaOfCircle1 to 6
+            // Calculates and prints area by calling a method.
+            areaOfCircle1.radius = 6;
+            areaOfCircle1.GetArea();
+
+            // Another object is created with radius 7
+            // Calculates and prints area
+            Circle areaOfCircle2 = new Circle(7);
+            areaOfCircle2.GetArea();
         }
     }
 }
